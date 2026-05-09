a18fe057741bcd39586cac89aebd1fa5b41174d1
Mohit Sahu <isdev03@gmail.com>
2026-05-10 01:10:44 +0530

    day 1

---
+++ program1.py
@@ -0,0 +1,3 @@
+userName = input()
+userAge = input()
+print("Hello "+userName+", next year you will be "+userAge+" years old")// why i can not concat int type of variable?
---
+++ program3.py
@@ -0,0 +1,14 @@
+sub1 = int(input())
+sub2 = int(input())
+sub3 = int(input())
+sub4 = int(input())
+sub5 = int(input())
+total = sub1+sub2+sub3+sub4+sub5
+print(f"Total {total}")
+percentage = total/5
+print(f"Percentage {percentage}")
+if total > 33 :
+    print("Pass")
+else:
+    print("Fail")
+
