ef942002834297a79cc25e7b36c3053213a64f4d
Mohit Sahu <isdev03@gmail.com>
2026-05-10 01:37:48 +0530

    fix structure 1

--- program1.py
+++
@@ -1,3 +0,0 @@
-userName = input()
-userAge = input()
-print("Hello "+userName+", next year you will be "+userAge+" years old")// why i can not concat int type of variable?
--- program2.py
+++
@@ -1,7 +0,0 @@
-def main():
-    number1 = int(input("Enter first number : "))
-    number2 = int(input("Enter second number : "))
-    print(f"Add {number1+number2}")    
-
-if __name__ == "__main__":
-    main()
